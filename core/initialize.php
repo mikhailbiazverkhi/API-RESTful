f5ca1fb9ad139f6b8ab7ae0d9e642e162729a907
Mikhail <mikhail.biazverkhi@gmail.com>
2023-08-02 12:07:52 -0400

    Comment initialize.php

--- core/initialize.php
+++ core/initialize.php
@@ -3,4 +3,6 @@ echo 'Hello, my entry point is the initialize.php in the /core folder';
 
 // require_once '../includes/config.php';
 
-require_once '../core/article.php';
+// require_once '../core/dao.php';
+
+// require_once '../core/article.php';
