58a6ee4327f845f940e66d800451ce85e0617788
Mikhail <mikhail.biazverkhi@gmail.com>
2023-08-02 10:15:08 -0400

    Add container.php

---
+++ container.php
@@ -0,0 +1,16 @@
+<?php
+use DI\Container;
+// use DI\ContainerBuilder;
+use Psr\Container\ContainerInterface;
+
+// Instanciez le conteneur de dépendances
+$container = new Container();
+// Configurer les dépendances
+// Exemple de configuration d'une dépendance pour la classe Article
+$container->set('Article', function (ContainerInterface $container) {
+    $db = $container->get('db');
+    return new Article($db);
+});
+// Ajoutez d'autres configurations de dépendances selon vos besoins
+// Retournez le conteneur
+return $container;
