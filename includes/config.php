c831097df0fb19754dfb102dc62cf8f85485b266
Mikhail <mikhail.biazverkhi@gmail.com>
2023-08-07 19:12:22 -0400

    Change config.php
    Add remote MySQL server settings

--- includes/config.php
+++ includes/config.php
@@ -1,9 +1,15 @@
 <?php
 
+//Remote MySQL Server db4free.net (for tests)
+$db_user = 'root_cegep';
+$db_password = 'FWt4#2cs6t';
+$db_name = 'tp2_blog_db';
+$conn = new PDO('mysql:host=db4free.net; dbname=' . $db_name . ';charset=utf8', $db_user, $db_password);
+
+//local
 $db_user = 'root';
 $db_password = '$Mike1q2w3e4R';
 $db_name = 'tp2_blog_db';
-
 $conn = new PDO('mysql:host=localhost; dbname=' . $db_name . ';charset=utf8', $db_user, $db_password);
 
 $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
